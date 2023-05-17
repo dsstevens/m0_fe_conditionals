861e01b1a63e3445d487b47a33ce4a2adb1ef16b
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-17 14:06:39 -0400

    Add answer to challenge

--- codeChallenges/mediumChallenge.js
+++ codeChallenges/mediumChallenge.js
@@ -2,7 +2,7 @@ var goodDrivingRecord = true;
 var age = 24;
 
 // Write a program that evaluates the status of the variables 
-// above, and uses logiv to pring one of three possible messages:
+// above, and uses logic to print one of three possible messages:
 
 // if the driving record is good and user is over 25 years old,
 // they should get a discount on the car rental
@@ -12,3 +12,13 @@ var age = 24;
 
 // if the user is not over 25 and has a bad driving record,
 // they need to have someone else sign for the rental
+
+if (goodDrivingRecord === true && age >= 25){
+    console.log("You get a car rental discount");
+  } else if (goodDrivingRecord === true || age >= 25){
+    console.log("You have to pay full price");
+  } else if (age < 25 && goodDrivingRecord === false){
+    console.log("Sorry, Someone else needs to sign for the rental.");
+  }
+  
+  
