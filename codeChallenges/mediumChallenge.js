6098060c97e742be02a381197c5a140f730810a7
Deanna Stevens <deanna.sofias@gmail.com>
2023-05-16 22:23:04 -0400

    Add instructions

---
+++ codeChallenges/mediumChallenge.js
@@ -0,0 +1,14 @@
+var goodDrivingRecord = true;
+var age = 24;
+
+// Write a program that evaluates the status of the variables 
+// above, and uses logiv to pring one of three possible messages:
+
+// if the driving record is good and user is over 25 years old,
+// they should get a discount on the car rental
+
+// if the user either has a good record or is over 25 years old,
+// they should pay full price
+
+// if the user is not over 25 and has a bad driving record,
+// they need to have someone else sign for the rental
